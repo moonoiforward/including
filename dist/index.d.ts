26f3aeb8a1a5cb13191102e5556686d435564bec
sarunjuntui77 <sarunjuntui@gmail.com>
2023-11-15 12:19:40 +0700

    0.0.15

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 export * from "./lib/including";
 export * from "./models/Action";
 export * from "./models/Include";
+export { including, IIncludingParam } from "./lib/including";
 //# sourceMappingURL=index.d.ts.map
